7ccbf2660f9e4f6c5674bad453bd38caf2296405
VIXO3029 <vrodriguez@ilernamadrid.com>
2023-04-02 20:45:07 +0200

    PRUEBAS

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,9 +9,13 @@
 import React from 'react';
 import '../styles/global.scss';
 
+
 const App = () => {
 	return (
+    
 		<h1>Hola Mundo, Bienvenidos al Curso Practico de React!</h1>
+    
+     
 	);
 }
 
